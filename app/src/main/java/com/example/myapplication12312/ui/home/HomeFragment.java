03d139f2d17fe646e97351b9349d90da4cccd9c9
bokunoheya <36652915+bokunoheya@users.noreply.github.com>
2020-03-13 16:24:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication12312/ui/home/HomeFragment.java
@@ -0,0 +1,43 @@
+package com.example.myapplication12312.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.myapplication12312.R;
+import com.example.myapplication12312.ui.slideshow.SlideshowFragment;
+
+public class HomeFragment extends Fragment {
+Button button12;
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        button12= root.findViewById(R.id.button2)       ;
+        button12.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_slideshow));
+        return root;
+    }
+}
